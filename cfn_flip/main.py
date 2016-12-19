4af9514883ffad6500f83f73696c2f47d4394447
Steve Engledow <sengledo@amazon.co.uk>
2016-12-19 13:11:34 +0000

    Add command-line switch for clean up option

--- cfn_flip/main.py
+++ cfn_flip/main.py
@@ -9,6 +9,7 @@ or in the "license" file accompanying this file. This file is distributed on an 
 """
 
 from . import flip
+import argparse
 import sys
 
 def main():
@@ -17,13 +18,18 @@ def main():
     Then figure out the input format and set the opposing output format
     """
 
-    ins = open(sys.argv[1], "r") if len(sys.argv) > 1 else sys.stdin
-    outs = open(sys.argv[2], "w") if len(sys.argv) > 2 else sys.stdout
+    # Set up the arg parser
+    parser = argparse.ArgumentParser(description="AWS CloudFormation Template Flip is a tool that converts AWS CloudFormation templates between JSON and YAML formats, making use of the YAML format's short function syntax where possible.")
+    parser.add_argument("-c", "--clean", action="store_true", help="Performs some opinionated cleanup on your template. For now, this just converts uses of Fn::Join to Fn::Sub.")
+    parser.add_argument("input", nargs="?", type=argparse.FileType("r"), default=sys.stdin, help="File to read from. If you do not supply a file, input will be read from stdin.")
+    parser.add_argument("output", nargs="?", type=argparse.FileType("w"), default=sys.stdout, help="File to write to. If you do not supply a file, output will be written to stdout.")
 
-    template = ins.read()
+    args = parser.parse_args()
+
+    template = args.input.read()
 
     try:
-        outs.write(flip(template))
+        args.output.write(flip(template, args.clean))
     except Exception as e:
         sys.stderr.write("{}\n".format(str(e)))
         sys.exit(1)
