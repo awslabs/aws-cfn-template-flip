ed728a0a951de84a70ca04fb0fe8eb027c99259e
David Lee <dlee@calldei.com>
2017-04-23 03:10:17 -0400

    Style and docstring
    As suggested.

--- cfn_flip/__init__.py
+++ cfn_flip/__init__.py
@@ -15,10 +15,14 @@ import collections
 import json
 
 class MyDumper(yaml.Dumper):
-
-  def increase_indent(self,flow=False,indentless=False):
-    return super(MyDumper,self).increase_indent(flow,False)
-
+  """
+  Indent block sequences from parent using more common style
+  ("  - entry"  vs "- entry").  
+  Causes fewer problems with validation and tools.
+  """
+  
+  def increase_indent(self,flow=False, indentless=False):
+    return super(MyDumper,self).increase_indent(flow, False)
 
 def to_json(template, clean_up=False):
     """
@@ -44,7 +48,7 @@ def to_yaml(template, clean_up=False):
     if clean_up:
         data = clean(data)
 
-    return yaml.dump(data, Dumper=MyDumper , default_flow_style=False)
+    return yaml.dump(data, Dumper=MyDumper, default_flow_style=False)
 
 def flip(template, clean_up=False):
     """
