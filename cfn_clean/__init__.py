462f84bcc0f440989b25625b0c7c185f1c303fc9
Rafael Koike <koiker@amazon.com>
2021-04-27 09:32:00 -0400

    Changed `cfn_clean/__init__.py` to import `collections.abc` only. (Compatible only with py3.3+) This change will keep the test coverage fully covered.

--- cfn_clean/__init__.py
+++ cfn_clean/__init__.py
@@ -4,7 +4,7 @@ Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License").
 You may not use this file except in compliance with the License. A copy of the License is located at
 
-    http://aws.amazon.com/apache2.0/
+    https://aws.amazon.com/apache2.0/
 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,11 +13,7 @@ See the License for the specific language governing permissions and limitations 
 
 from cfn_tools.odict import ODict
 from cfn_tools.literal import LiteralString
-
-try:
-    from collections.abc import KeysView
-except ImportError:
-    from collections import KeysView
+from collections.abc import KeysView
 
 import json
 import six
